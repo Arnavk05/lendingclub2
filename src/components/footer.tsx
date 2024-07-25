4a83e6486c9d5d45da4f82a6fe522324782d98a2
Arnav Kumar <Arnavk05@gmail.com>
2024-07-25 15:51:53 +0530

    Add files via upload

---
+++ src/components/footer.tsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <>
+      <footer className="border-top p-4">
+        <div className="copyright text-center text-sm text-muted">
+          Get in touch:
+          
+        </div>
+      </footer>
+    </>
+  );
+}
+
+
